1f98538e9a59653f28449274b0a713165c69af28
flippedround <734243792@qq.com>
2025-11-19 12:07:07 +0800

    fix(terminal): 更新终端输出过滤和替换规则

--- src/constants/terminal.ts
+++ src/constants/terminal.ts
@@ -3,6 +3,7 @@ import { getPackageInfoSync } from 'local-pkg'
 import moment from 'moment'
 
 export const TERMINAL_SKIP_OUTPUTS = [
+  '编译器版本',
   '正在编译中...',
   '开始差量编译...',
   '请注意运行模式下，因日志输出、sourcemap 以及未压缩源码等原因，性能和包体积，均不及发行模式。',
@@ -14,7 +15,7 @@ export const TERMINAL_REPLACE_OUTPUTS = [
   //   to: '使用 unh update 命令升级到最新版本\n',
   // },
   {
-    from: 'DONE  Build complete.',
+    from: 'Watching for changes',
     to: () => `${gray(moment().format('hh:mm:ss A'))} ${cyan(bold('[vite]'))} ${green('hmr update')}\n`,
   },
   {
